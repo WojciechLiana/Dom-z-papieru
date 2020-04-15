0e8036c0c50ac71c2b4ddcef62b177bf16322e76
Wojciech Liana <wojtek.liana@gmail.com>
2020-04-15 13:09:16 +0200

    webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./main.css";
+import "./bootstrap/css/bootstrap.min.css"
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+        bootstrap: './src/bootstrap/js/bootstrap.min.js'
+    },
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    plugins: [new HtmlWebPackPlugin({template: 'index.html'})],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.exec\.js$/,
+                use: [ 'script-loader' ]
+            }
+        ]
+    },
+};
